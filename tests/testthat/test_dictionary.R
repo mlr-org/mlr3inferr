508bacd0c49137ee652d3319fe45bb2e265545a9
Sebastian Fischer <sebf.fischer@gmail.com>
2024-07-17 15:34:40 +0200

    trigger ci

--- tests/testthat/test_dictionary.R
+++ tests/testthat/test_dictionary.R
@@ -1,4 +1,3 @@
 test_that("measure dictionary can still be converted to table", {
   expect_data_table(as.data.table(mlr3::mlr_measures))
-  # TODO: Check that all IDs are also properly represented
 })
