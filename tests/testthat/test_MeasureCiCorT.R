5ad545333e51472ed0b039fbd5544b286e038cbc
Sebastian Fischer <sebf.fischer@gmail.com>
2025-08-28 16:49:40 +0200

    add another test

--- tests/testthat/test_MeasureCiCorT.R
+++ tests/testthat/test_MeasureCiCorT.R
@@ -1,3 +1,11 @@
 test_that("simple", {
   expect_ci_measure("ci.cor_t", rsmp("subsampling", repeats = 10L))
 })
+
+test_that("aggr and CI point estimate agree", {
+  task = tsk("iris")
+  rr = resample(task, lrn("classif.featureless"), rsmp("subsampling", repeats = 10L))
+  ci = rr$aggregate(msr("ci", "classif.acc"))
+  aggr = rr$aggregate(msr("classif.acc"))
+  expect_equal(ci[[1L]], aggr[[1L]])
+})
